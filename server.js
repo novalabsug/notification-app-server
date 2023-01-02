08ba00f7b327a64fe35dd530ec420db16d3e1fe2
novalabsug <novalabsug@gmail.com>
2023-01-02 09:20:24 +0300

    fixing server app issues. edited stackoverflow answer

--- server.js
+++ server.js
@@ -26,7 +26,10 @@ app.use((req, res, next) => {
   if (allowedOrigins.includes(origin)) {
     res.setHeader("Access-Control-Allow-Origin", origin);
   }
-  //res.header('Access-Control-Allow-Origin', 'http://127.0.0.1:8020');
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://mail-app-frontend.netlify.app"
+  );
   res.header("Access-Control-Allow-Methods", "GET, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.header("Access-Control-Allow-Credentials", true);
